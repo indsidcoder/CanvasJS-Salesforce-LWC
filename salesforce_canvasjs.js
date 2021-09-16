f7b20e0b00af6c8040accf24b3d68f54ce437fec
indranil1991 <isinghdeo@gmail.com>
2021-09-16 22:34:56 +0530

    Updated lifecycle hook in JS file

--- salesforce_canvasjs.js
+++ salesforce_canvasjs.js
@@ -7,41 +7,37 @@ export default class Salesforce_canvasjs extends LightningElement {
     canvasjsInitialized = false;
     chart;
     error;
-
-    connectedCallback() {
+    renderedCallback() {
         if (this.canvasjsInitialized) {
             return;
         }
         this.canvasjsInitialized = true;
 
-        loadScript(this, CanvasJS)
-            .then(() => {
-
-                const container = this.template.querySelector('[data-id="chartContainer"]');
-                this.chart = new window.CanvasJS.Chart(container, {
-                    animationEnabled: true,
-                    title: {
-                        text: "Basic Column Chart in Salesforce LWC"
-                    },
-                    data: [{
-                        type: "column",
-                        dataPoints: [
-                            { y: 41, label: "Apple" },
-                            { y: 55, label: "Mango" },
-                            { y: 50, label: "Orange" },
-                            { y: 65, label: "Banana" },
-                            { y: 95, label: "Pineapple" },
-                            { y: 68, label: "Pears" },
-                            { y: 28, label: "Grapes" },
-                            { y: 34, label: "Lychee" },
-                            { y: 14, label: "Jackfruit" }
-                        ]
-                    }]
-                });
-                this.chart.render();
-            })
-            .catch((error) => {
-                this.error = error;
+        loadScript(this, CanvasJS).then(() => {
+            const container = this.template.querySelector('[data-id="chartContainer"]');
+            this.chart = new window.CanvasJS.Chart(container, {
+            animationEnabled: true,
+                title: {
+                    text: "Basic Column Chart in Salesforce LWC"
+                },
+                data: [{
+                    type: "column",
+                    dataPoints: [
+                        { y: 41, label: "Apple" },
+                        { y: 55, label: "Mango" },
+                        { y: 50, label: "Orange" },
+                        { y: 65, label: "Banana" },
+                        { y: 95, label: "Pineapple" },
+                        { y: 68, label: "Pears" },
+                        { y: 28, label: "Grapes" },
+                        { y: 34, label: "Lychee" },
+                        { y: 14, label: "Jackfruit" }
+                    ]
+                }]
             });
+            this.chart.render();
+        }).catch((error) => {
+            this.error = error;
+        });
     }
 }
